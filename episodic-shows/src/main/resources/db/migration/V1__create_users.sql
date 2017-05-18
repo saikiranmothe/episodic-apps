b0b0e0df254fdf98f262e6ae9439e8cc5185677d
saikiranmothe <saikiran.mothe@gmail.com>
2017-05-18 13:42:48 -0600

    updated gradle properties

--- episodic-shows/src/main/resources/db/migration/V1__create_users.sql
+++
@@ -1,4 +0,0 @@
-create table users (
-  id bigint not null auto_increment primary key,
-  email varchar(255) not null unique
-);
--- episodic-shows/src/test/java/com/example/users/UsersControllerTest.java
+++ episodic-shows/src/test/java/com/example/users/UsersControllerTest.java
@@ -25,6 +25,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
